20195653c6e3de4fff0a59f3b83827064db73aba
Julio Galindo <jgalindoc@gmail.com>
2015-10-17 12:24:07 -0600

    added average

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,11 @@ class Review < ActiveRecord::Base
 	
 	validates_presence_of :score
 	validates_presence_of :content
+
+	after_save :calculate_average
+
+	def calculate_average
+		a = self.place.avererage_rating
+		self.place.update_attribute(total_average_rating: a)
+	end 
 end
